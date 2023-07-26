ff43a3a256231e72fc171139b6039db173bc1f1e
Charles <137882382+ClsApps@users.noreply.github.com>
2023-07-26 13:23:29 +0300

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
+                url: "https://clsapps.github.io/Fix/contact.php",
                 type: "POST",
                 data: {
                     name: name,
